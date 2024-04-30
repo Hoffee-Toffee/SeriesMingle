81a0da798ff99b25c187f14d0f01f6d4daa4f5ec
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-04-30 17:07:18 +1200

    Route test

--- server/server.ts
+++ server/server.ts
@@ -21,10 +21,11 @@ if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use(
     '/assets',
-    express.static(Path.resolve(__dirname, 'dist', 'assets')),
+    express.static(Path.resolve(__dirname, '../dist', 'assets')),
   )
   server.get('*', (req, res) => {
-    res.sendFile(Path.resolve(__dirname, 'dist', 'index.html'))
+    res.send(Path.resolve(__dirname, 'dist', 'index.html'))
+    // res.sendFile(Path.resolve(__dirname, '../dist', 'index.html'))
   })
 }
 
