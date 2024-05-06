c50729ee0fccadde9d6bc9011cd0a4e52f0d6744
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-05-07 10:19:57 +1200

    Route fix attempt 2

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import routes from './routes.tsx'
-const router = createBrowserRouter(routes)
+import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(
-    <RouterProvider router={router} />,
-  )
+  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
 })
