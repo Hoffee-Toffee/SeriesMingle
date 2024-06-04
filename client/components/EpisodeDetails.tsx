55c683b89a5ca5fafc2231aed907a0ce71d998ce
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-06-05 00:04:27 +1200

    Movie groups with a title and only one entry will use that as an alternate title for that movie

--- client/components/EpisodeDetails.tsx
+++ client/components/EpisodeDetails.tsx
@@ -1,9 +1,18 @@
 export default function EpisodeDetails(props) {
-  const { entry, i, schedule, colors, numberOfLayers, bookmark, setBookmark, last } = props
+  const {
+    entry,
+    i,
+    schedule,
+    colors,
+    numberOfLayers,
+    bookmark,
+    setBookmark,
+    last,
+  } = props
 
   const minHeight = 30
 
-  const posId = `${entry.layer}-${entry.layer_id}${entry.type == 'movie' ? '' : '-' + (entry.type == 'episode' ? entry.id : colors.custom[entry.set].indices.findIndex(e => e == i))}`
+  const posId = `${entry.layer}-${entry.layer_id}${entry.type == 'movie' ? '' : '-' + (entry.type == 'episode' ? entry.id : colors.custom[entry.set].indices.findIndex((e) => e == i))}`
 
   return (
     <div
@@ -22,22 +31,22 @@ export default function EpisodeDetails(props) {
         marginLeft: `${entry.layer * 28 + 3}px`,
         backgroundColor: `hwb(${entry.show_id ? colors.tv[entry.show_id].color : entry.type == 'movie' ? colors.movie[entry.layer].color : colors.custom[entry.set].color} 0% 25%)`,
         marginTop: `calc(var(--scale) * ${Math.max(0, minHeight - (entry.runtime || entry.average_run_time)) + 30}px / 2)`,
-        marginBottom: `calc(var(--scale) * ${Math.max(0, minHeight - (entry.runtime || entry.average_run_time)) + 30}px / 2)`
+        marginBottom: `calc(var(--scale) * ${Math.max(0, minHeight - (entry.runtime || entry.average_run_time)) + 30}px / 2)`,
       }}
       id={bookmark == posId ? 'bookmark' : undefined}
     >
       <div
         className={['episode-detail', i % 2 && 'odd'].filter(Boolean).join(' ')}
         style={{
-          minHeight: `${(Math.max(minHeight, (entry.runtime || entry.average_run_time))) + 4}px`,
-          maxHeight: `${(Math.max(minHeight, (entry.runtime || entry.average_run_time))) + (schedule[i + 1] && Math.max(minHeight, (schedule[i + 1].runtime || schedule[i + 1].average_run_time))) + 20}px`,
+          minHeight: `${Math.max(minHeight, entry.runtime || entry.average_run_time) + 4}px`,
+          maxHeight: `${Math.max(minHeight, entry.runtime || entry.average_run_time) + (schedule[i + 1] && Math.max(minHeight, schedule[i + 1].runtime || schedule[i + 1].average_run_time)) + 20}px`,
           width: `calc(50% * var(--scale) - ${numberOfLayers} * var(--layer-size) * var(--scale) - var(--offset))`,
         }}
       >
         <div
           style={{
-            minHeight: `${(Math.max(minHeight, (entry.runtime || entry.average_run_time))) - 12}px`,
-            maxHeight: `calc(var(--scale) * ${(Math.max(minHeight, (entry.runtime || entry.average_run_time))) - 2}px + var(--height-mult) * ${(schedule[i + 1] ? Math.max(minHeight, (schedule[i + 1].runtime || schedule[i + 1].average_run_time)) : 40) - 2}px)`,
+            minHeight: `${Math.max(minHeight, entry.runtime || entry.average_run_time) - 12}px`,
+            maxHeight: `calc(var(--scale) * ${Math.max(minHeight, entry.runtime || entry.average_run_time) - 2}px + var(--height-mult) * ${(schedule[i + 1] ? Math.max(minHeight, schedule[i + 1].runtime || schedule[i + 1].average_run_time) : 40) - 2}px)`,
           }}
         >
           <label htmlFor={`bookmark-${i}`}>
@@ -55,16 +64,34 @@ export default function EpisodeDetails(props) {
           />
           {entry.show_id ? (
             <span>
-              {colors.tv[entry.show_id].userTitle || entry.show_title} (S{entry.season}E{entry.episode})
+              {colors.tv[entry.show_id].userTitle || entry.show_title} (S
+              {entry.season}E{entry.episode})
             </span>
-          ) : entry.type == 'movie' && colors.movie[entry.layer].userTitle && <span>
-            {colors.movie[entry.layer].userTitle} ({colors.movie[entry.layer].indices.findIndex(e => e == i) + 1} of {colors.movie[entry.layer].indices.length})
+          ) : (
+            entry.type == 'movie' &&
+            colors.movie[entry.layer].userTitle &&
+            colors.movie[entry.layer].indices.length > 1 && (
+              <span>
+                {colors.movie[entry.layer].userTitle} (
+                {colors.movie[entry.layer].indices.findIndex((e) => e == i) + 1}{' '}
+                of {colors.movie[entry.layer].indices.length})
+              </span>
+            )
+          )}
+          <span className="title">
+            {entry.type == 'custom'
+              ? `${entry.title}${colors.custom[entry.set].indices.length > 1 || (entry.offset && entry.offset > 0) ? ` (${entry.term} ${colors.custom[entry.set].indices.findIndex((e) => e == i) + 1 + (entry.offset || 0)})` : ''}`
+              : entry.type == 'movie' &&
+                colors.movie[entry.layer].userTitle &&
+                colors.movie[entry.layer].indices.length == 1
+                ? colors.movie[entry.layer].userTitle || entry.title
+                : entry.title}
           </span>
-          }
-          <span className="title">{entry.type == 'custom' ? `${entry.title}${(colors.custom[entry.set].indices.length > 1 || (entry.offset && entry.offset > 0)) ? ` (${entry.term} ${colors.custom[entry.set].indices.findIndex(e => e == i) + 1 + (entry.offset || 0)})` : ''}` : entry.title}</span>
-          {entry.type !== 'custom' && <span className="spoiler">
-            {entry.overview || 'No Description Available'}
-          </span>}
+          {entry.type !== 'custom' && (
+            <span className="spoiler">
+              {entry.overview || 'No Description Available'}
+            </span>
+          )}
         </div>
       </div>
     </div>
