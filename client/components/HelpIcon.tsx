7c277e4114963902622e3edac9a977efb7190d2a
Hoffee-Toffee <Tristan-Bulmer@live.nmit.ac.nz>
2024-06-06 22:27:18 +1200

    Help icon hidden for now

--- client/components/HelpIcon.tsx
+++ client/components/HelpIcon.tsx
@@ -1,4 +1,6 @@
 export default function HelpIcon(props) {
+  return <></> // Temporary
+
   const { info } = props
   return <i className="fa-regular fa-circle-question help">
     <span className="info">{info}</span>
