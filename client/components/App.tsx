0c6a90935e3a70d11d8184efb207dcb0536d0960
Hoffee Toffee <tb59391@gmail.com>
2024-06-29 23:39:44 +1200

    App component maintains contexts

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,40 @@
 import { Helmet } from 'react-helmet'
 import favicon from '../files/favicon.ico'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import { createContext, useEffect, useState } from 'react'
+
+import { auth } from '../../server/firebase.ts'
 
 export default function App() {
   const url = new URL(window.location.href)
   const id = url.searchParams.get('id')
+  const location = useLocation().key
+
+  const [user, setUser] = useState(auth.currentUser)
+  const [isAuthLoaded, setIsAuthLoaded] = useState(false)
+  const [isPageLoaded, setIsPageLoaded] = useState(false)
+
+  const [showLoading, setShowLoading] = useState(false)
+  const hasLoaded = isAuthLoaded && isPageLoaded
+
+  const loader = hasLoaded ? null : setTimeout(() => {
+    setShowLoading(true)
+  }, 2000)
+
+  useEffect(() => {
+    return auth.onAuthStateChanged((user) => {
+      setUser(user || null)
+      setIsAuthLoaded(true)
+      if (!showLoading) clearTimeout(loader)
+    })
+  }, [loader, showLoading])
+
+  // Reset the loading on location change
+  useEffect(() => {
+    return () => {
+      setIsPageLoaded(false)
+    }
+  }, [location])
 
   return (
     <>
@@ -24,8 +54,23 @@ export default function App() {
           />
         }
       </Helmet>
-      <Outlet />
-      {/* Try without outlet */}
+      {showLoading &&
+        <div id="loading" className={hasLoaded ? 'loaded' : 'loading'}>
+          <div className="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+          <span className="scanline"></span>
+        </div>}
+      {isAuthLoaded &&
+        <UserContext.Provider value={{ user, setUser }}>
+          <LoadingContext.Provider value={{ isPageLoaded, setIsPageLoaded }}>
+            <Outlet />
+          </LoadingContext.Provider>
+        </UserContext.Provider>}
     </>
   )
 }
+
+export const UserContext = createContext({})
+export const LoadingContext = createContext({})
