5135c1cc994845eece14c065fbe9f14b26a59161
Hoffee Toffee <tb59391@gmail.com>
2024-06-29 23:41:47 +1200

    New routes added for dashboard and projects

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,12 +1,30 @@
 import { Route, createRoutesFromElements } from 'react-router-dom'
 import App from './components/App.tsx'
+import Dashboard from './components/Dashboard.tsx'
 import Project from './components/Project.tsx'
+import Home from './components/Home.tsx'
+import Login from './components/Login.tsx'
+import NotFound from './components/NotFound.tsx'
+
+import ProtectedRoute from './components/ProtectedRoute.tsx'
 
 export default createRoutesFromElements(
   <>
     <Route path="/" element={<App />}>
-      <Route index element={<Project />} />
-      <Route path="ping" element={<p>Pong!</p>} />
+      {/* <Route index element={<Home />} /> */}
+      <Route index element={<ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>} />
+      <Route path="login" element={<ProtectedRoute ifLoggedIn={false} redirectTo="/dashboard">
+        <Login />
+      </ProtectedRoute>} />
+      <Route path="dashboard" element={<ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>} />
+      <Route path="project/:id" element={<ProtectedRoute>
+        <Project />
+      </ProtectedRoute>} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   </>,
 )
