42d38322dfc9949d0279d3db52ab7b0e210a23d3
Hoffee Toffee <tb59391@gmail.com>
2024-08-20 17:32:53 +1200

    Vite config edit test

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,19 @@ export default defineConfig({
       '/api': 'http://localhost:3000',
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
+      },
+    },
+  },
 })
