634b0acc3bef9d87db153ce59e9652706913dbc3
Hoffee Toffee <tb59391@gmail.com>
2024-10-16 19:05:34 +1300

    Fixed SEO

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import * as Path from 'node:path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
+import Index from './index.jsx'
 
 // Get the directory name of the current module file
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -27,58 +28,9 @@ server.get('/api/v1/secret', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve(__dirname, 'assets')))
-  server.get('*', (req, res) => {
-    let title = ['SeriesMingle']
-    let description =
-      'Create and edit your own TV and Movie schedules with SeriesMingle.'
-
-    const path = req.path.split('/')[1].toLowerCase()
-
-    if (['login, dashboard', 'project'].includes(path))
-      title.unshift(path.charAt(0).toUpperCase() + path.slice(1))
-
-    if (path === 'project')
-      description = 'Check out my TV and movie schedule on SeriesMingle!'
-
-    title = title.join(' | ')
-
-    res.setHeader('Content-Type', 'text/html')
-    res.send(`<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script
-      src="https://kit.fontawesome.com/251912c541.js"
-      crossorigin="anonymous"
-    ></script>
-    <title>${title}</title>
-    <link
-      href="./client/files/favicon.ico"
-      rel="shortcut icon"
-      type="image/x-icon"
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="${title}" />
-    <meta
-      property="og:description"
-      content="${description}"
-    />
-    <meta
-      property="og:url"
-      content="https://seriesmingle.com"
-    />
-    <meta
-      property="og:image"
-      content="https://seriesmingle.com/assets/icon.png"
-    />
-  </head>
-  <body>
-    <div id="app"></div>
-    <script src="./client/index.tsx" type="module"></script>
-  </body>
-</html>`)
-  })
+  server.get('*', (req, res) =>
+    res.sendFile(Path.resolve(__dirname, 'index.html')),
+  )
 }
 
 export default server
