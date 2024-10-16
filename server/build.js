244e57f9179ffb7d85379fe1b5c58623dd7a0437
Hoffee Toffee <tb59391@gmail.com>
2024-10-16 23:01:37 +1300

    Robots and Sitemap copied to Dist

---
+++ server/build.js
@@ -0,0 +1,40 @@
+import * as esbuild from 'esbuild'
+import fs from 'fs'
+import * as Path from 'node:path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const rootDir = Path.join(__dirname, '..')
+
+esbuild
+  .build({
+    entryPoints: ['server/index.ts'],
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    outfile: 'dist/server.js',
+    external: ['express'],
+  })
+  .then(() => {
+    console.log('Build complete')
+
+    // Copy 'robots.txt' and 'sitemap.xml' to the 'dist' folder
+    try {
+      const robotsSrc = Path.join(rootDir, 'robots.txt')
+      const robotsDest = Path.join(rootDir, 'dist/robots.txt')
+      fs.copyFileSync(robotsSrc, robotsDest)
+      console.log(`Copied robots.txt from ${robotsSrc} to ${robotsDest}`)
+
+      const sitemapSrc = Path.join(rootDir, 'sitemap.xml')
+      const sitemapDest = Path.join(rootDir, 'dist/sitemap.xml')
+      fs.copyFileSync(sitemapSrc, sitemapDest)
+      console.log(`Copied sitemap.xml from ${sitemapSrc} to ${sitemapDest}`)
+    } catch (error) {
+      console.error('Error copying files:', error)
+    }
+  })
+  .catch((error) => {
+    console.error('Build failed:', error)
+    process.exit(1)
+  })
--- server/server.ts
+++ server/server.ts
@@ -28,9 +28,6 @@ server.get('/api/v1/secret', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve(__dirname, 'assets')))
-  server.get('/favicon.ico', (req, res) =>
-    res.sendFile(Path.resolve(__dirname, 'favicon.ico')),
-  )
   server.get('/robots.txt', (req, res) =>
     res.sendFile(Path.resolve(__dirname, 'robots.txt')),
   )
