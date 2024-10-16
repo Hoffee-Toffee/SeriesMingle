3bcbe613aae8337d26199d7eb775e3a5af3bf4ee
Hoffee Toffee <tb59391@gmail.com>
2024-10-16 23:35:59 +1300

    Copy fix

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,22 @@ import dotenv from 'dotenv'
 dotenv.config()
 import server from './server.ts'
 
+import fs from 'fs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import * as Path from 'node:path'
+
+const __dirname = Path.join(dirname(fileURLToPath(import.meta.url)), '..')
+
+;['robots.txt', 'sitemap.xml'].forEach((file) =>
+  fs.copyFileSync(
+    Path.resolve(__dirname, file),
+    Path.resolve(__dirname, 'dist', file),
+  ),
+)
+
 const PORT = process.env.PORT || 3000
 
-// Start the server only if this module is the main module
 server.listen(PORT, () => {
   console.log('Server listening on port', PORT)
 })
