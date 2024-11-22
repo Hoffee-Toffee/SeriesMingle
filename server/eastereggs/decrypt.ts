53df951eb15cef273bfd32517d55243ea0cc00c0
Hoffee Toffee <tb59391@gmail.com>
2024-11-22 13:20:05 +1300

    Updated Easteregg Decryption

--- server/eastereggs/decrypt.ts
+++ server/eastereggs/decrypt.ts
@@ -2,11 +2,10 @@ import cryptoPKG from 'crypto'
 import fs from 'fs'
 import * as Path from 'node:path'
 import { fileURLToPath } from 'url'
-import { join } from 'node:path'
+import { dirname } from 'path'
 
 // Get the directory name of the current module file
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = join(__filename, '../../server/eastereggs')
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default function decrypt(passphrase = 'test123') {
   // Must try it on all txt files in the directory, and return the one that works
@@ -25,9 +24,13 @@ export default function decrypt(passphrase = 'test123') {
 
 function decryptFile(componentName = 'test', passphrase = 'test123') {
   const encodedFile = Path.resolve(__dirname, `${componentName}.txt`)
+  const data = fs.readFileSync(encodedFile, 'utf8')
 
-  const encrypted = fs.readFileSync(encodedFile, 'utf8')
-  const decipher = cryptoPKG.createDecipher('aes-256-ctr', passphrase)
+  const [ivHex, encrypted] = data.split(':::')
+  const iv = Buffer.from(ivHex, 'hex')
+  const key = cryptoPKG.scryptSync(passphrase, 'salt', 32)
+  const decipher = cryptoPKG.createDecipheriv('aes-256-ctr', key, iv)
+
   let decrypted = decipher.update(encrypted, 'hex', 'utf8')
   decrypted += decipher.final('utf8')
 
